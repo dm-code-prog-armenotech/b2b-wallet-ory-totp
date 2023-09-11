b400374c995e05193f246428da061fb643ef8909
dm-code-prog-armenotech <124766379+dm-code-prog-armenotech@users.noreply.github.com>
2023-09-11 12:18:51 +0300

    Check session

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export async function middleware(request: NextRequest) {
+  
+  const session = cookies().get('ory_kratos_session');
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', request.nextUrl).toString());
+  }
+  
   const res = await fetch('https://kratos-totp-demo.fly.dev/sessions/whoami', {
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Cookie': `ory_kratos_session=${session}`
     }
   });
   if (res.status === 401) {
