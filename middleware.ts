65e2908bf9a37f802255eefe2fcf4e38b13f606e
dm-code-prog-armenotech <124766379+dm-code-prog-armenotech@users.noreply.github.com>
2023-09-11 14:29:49 +0300

    fixes

--- middleware.ts
+++ middleware.ts
@@ -4,22 +4,28 @@ export async function middleware(request: NextRequest) {
   const isLogin = request.nextUrl.pathname === '/login';
   
   const session = request.cookies.get('ory_kratos_session');
-  if (!session || !isLogin) {
-    console.log('[middleware] session', 'session not found');
+  if (!session && !isLogin) {
     return NextResponse.redirect(new URL('/login', request.nextUrl).toString());
   }
   
-  const res = await fetch('https://kratos-totp-demo.fly.dev/sessions/whoami', {
-    headers: {
-      'Content-Type': 'application/json',
-      'Cookie': `ory_kratos_session=${session.value}`
+  if (!session && isLogin) {
+    return NextResponse.next();
+  }
+  
+  if (session) {
+    const res = await fetch('https://kratos-totp-demo.fly.dev/sessions/whoami', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': `ory_kratos_session=${session.value}`
+      }
+    });
+    
+    if (res.status === 401 && !isLogin) {
+      console.log('[middleware]', 'session invalid');
+      return NextResponse.redirect(new URL('/login', request.nextUrl).toString());
     }
-  });
-  
-  if (res.status === 401 && !isLogin) {
-    console.log('[middleware]', 'session invalid');
-    return NextResponse.redirect(new URL('/login', request.nextUrl).toString());
   }
+  
   
   return NextResponse.next();
 }
