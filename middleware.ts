26d9a843023ed374cb9f614c20890e1a55b8ab7d
dm-code-prog-armenotech <124766379+dm-code-prog-armenotech@users.noreply.github.com>
2023-09-11 13:20:51 +0300

    Fix infinite redirects again

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,8 @@ export async function middleware(request: NextRequest) {
       'Cookie': `ory_kratos_session=${session.value}`
     }
   });
-  if (res.status === 401) {
+  
+  if (res.status === 401 && !isLogin) {
     console.log('[middleware]', 'session invalid');
     return NextResponse.redirect(new URL('/login', request.nextUrl).toString());
   }
