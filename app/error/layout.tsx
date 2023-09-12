7931649a53212ff6a2a9e5f9cd145181df94777b
dm-code-prog-armenotech <124766379+dm-code-prog-armenotech@users.noreply.github.com>
2023-09-12 11:51:43 +0300

    Added the layout for the root page

---
+++ app/error/layout.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+
+export const metadata = {
+  title: 'Ory TOTP demo | Error',
+  description: 'A demo dashboard type of app, that provides 2fa with Google Authenticator, powered by Ory Kratos'
+};
+
+export default function RootLayout({
+                                     children
+                                   }: {
+  children: ReactNode
+}) {
+  return (
+    <html lang='en'>
+    <body>{children}</body>
+    </html>
+  );
+}
