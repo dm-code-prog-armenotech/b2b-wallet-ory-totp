507651f037d6f5b04aafdea53eff4673c495e0ce
dm-code-prog-armenotech <124766379+dm-code-prog-armenotech@users.noreply.github.com>
2023-09-12 12:19:15 +0300

    Get flow error on the error page

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,39 @@
+'use client';
+
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+import { Callout } from '@tremor/react';
+import { kratos } from '../../lib/kratos';
+import { ReactNode } from 'react';
+
 export default function Page() {
-  return null;
+  
+  const query = useQuery(['get', 'user', 'facing', 'error'],
+    async () => {
+      const queryParams = new URLSearchParams(window.location.search);
+      const id = queryParams.get('id');
+      const { data } = await kratos.getFlowError({
+        id: id || ''
+      });
+      return data;
+    }
+  );
+  
+  let children: ReactNode = '';
+  
+  if (query.isLoading) {
+    children = 'Loading...';
+  }
+  
+  if (query.isSuccess) {
+    children = JSON.stringify(query.data.error, null, 2);
+  }
+  
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <Callout title={'Error'} className={'w-[380px]'}>
+        {children}
+      </Callout>
+    </QueryClientProvider>
+  );
 }
