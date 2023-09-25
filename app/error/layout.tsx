6fe12300bbee16031a23296eef7e4d0363521a52
dm-code-prog-armenotech <124766379+dm-code-prog-armenotech@users.noreply.github.com>
2023-09-25 13:33:36 +0400

    Fix the error page layout

--- app/error/layout.tsx
+++ app/error/layout.tsx
@@ -3,17 +3,14 @@ import '../globals.css';
 
 export const metadata = {
   title: 'Ory TOTP demo | Error',
-  description: 'A demo dashboard type of app, that provides 2fa with Google Authenticator, powered by Ory Kratos'
+  description:
+    'A demo dashboard type of app, that provides 2fa with Google Authenticator, powered by Ory Kratos'
 };
 
-export default function RootLayout({
-                                     children
-                                   }: {
-  children: ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
-    <body>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
